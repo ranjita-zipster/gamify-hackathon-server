3a4e3b296354ecf5d3307225abbe01c9839b5c7c
Ranjita <rbhattachaeyya@zipcar.com>
2020-12-10 11:28:26 -0800

    fix cors issue

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3001;
 const routes = require('./src/events/addEvents');
 var cors = require('cors');
 
+app.options('*', cors()) ;
 app.use(cors());
 
 app.use(bodyParser.urlencoded({
@@ -22,6 +23,6 @@ app.get('/', (req, res) => {
 
 
 app.use('/events', routes);
-// Start the server on port 3000
+// Start the server on port 3001
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
-console.log('Node server running on port 3000');
+console.log('Node server running on port 3001');
